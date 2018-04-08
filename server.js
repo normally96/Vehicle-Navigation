a78c807425f6880ccdfca76fa4f7105d88c03fb7
Dung Hoang <dunghv@dgroup.co>
2018-04-08 14:56:38 +0700

    Update DB connection

--- server.js
+++ server.js
@@ -1,52 +1,56 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
+const SocketIO = require('socket.io');
 
-let app = express();
+require('./connection.js');
+
+const app = express();
+
+let myDB = null;
+
+app.use(cors());
 
 app.use('/public', express.static('public'));
+
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/ChartJsTest.html');    // gửi file html lên máy
 });
- var cors = require('cors');
- app.use(cors());
-let server = http.Server(app);
-server.listen(process.env.PORT || 80, function(){
-  console.log("socket server connected...")
-});
 
-let io = require('socket.io')(server);       // khởi tạo thư viện socket.io
+// create a server for Socket.io
+const server = http.Server(app);
 
-io.on('connection',function(socket){			// đoạn chương trình sẽ chạy khi có một kết nối đến server 
+// khởi tạo thư viện socket.io
+const io = SocketIO(server);
+
+io.on('connection',function(socket) {			// đoạn chương trình sẽ chạy khi có một kết nối đến server 
 	console.log('socket connection');			// in ra màn hình để debug
 	socket.on('customEvent',function(msg){				//nếu nhận một tin nhắm với mã là Nut1 thì sẽ xử lý tín hiệu msg
 		socket.broadcast.emit('message','heloo arduino');
 		console.log('ok web');
-		MongoClient.connect(url, function(err, db) {			// hàm connect trực tiếp với database
- 		 if (err) throw err;
-  		var dbo = db.db("mydb"); 								//
-  			//var myobj = { name: "Company Inc", address: "Highway 38" };
-  			dbo.collection("customers").insertOne(msg, function(err, res) { // kết nối vào collection customers và inser message từ website vào database
-    		if (err) throw err;
-    		console.log("1 document inserted");
-    		db.close();
-  			});
+		// insert sample data to DB
+		myDB.collection("customers").insertOne({ message: msg }, function(err, res) { // kết nối vào collection customers và inser message từ website vào database
+			if (!err) {
+				console.log('inserted successfully!');
+				console.log(res);
+			}
 		});
 	});
 
 	socket.on('rpm',function(msg){
-		MongoClient.connect(url, function(err, db) {
- 		 if (err) throw err;
-  		var dbo = db.db("mydb");
-  		dbo.collection("customers").find({}).toArray(function(err, result){  //lấy tất cả file trong collection customers
-   		 if (err) throw err;
-   		 console.log(result);     // lấy kết quả in lên màn hình
-  		  db.close();
-  			});
-}		);
+		myDB.collection("customers").find({}).toArray(function(err, result){  //lấy tất cả file trong collection customers
+			if (!err) {
+				console.log(result);
+			}
+		});
 		console.log('ok rpm');
 	});
-})
+});
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
 
+process.once('dbReady', () => {
+	myDB = global.connection.db('normally');
+	server.listen(process.env.PORT || 3000, () => {
+		console.log('Server started on port 3000...');
+	});
+});
