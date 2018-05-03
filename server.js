aea6eb76435828c047957c64f4f896651babe894
Hieu <normally96@gmail.com>
2018-05-03 22:10:00 +0700

    make clear code

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const SocketIO = require('socket.io');
-var net = require('net');			// add module net
+const express = require('express'); // add express module
+const http = require('http');       // add http module
+const cors = require('cors');       // add cors module
+const SocketIO = require('socket.io'); // add socket.io module
+var net = require('net'); // add module net
+
+// tạo server TCP/IP
 var tcpServer = net.createServer();
 
-tcpServer.on("connection", function(socket){
-	console.log("tcpServer connected");
-	socket.on("data",function(msg){
-		var message = msg.toString();
-		var myobj= JSON.parse(message);
-		global.socketIO.emit('liveUpdates',myobj); // gửi tệp vừa đóng gói lên trang liveUpdates
-		//insert sample data to DB
-		myDB.collection("status").insertOne(myobj, function(err, res) { // kết nối vào collection status và insert message từ website vào database
-			if (!err) {
-				console.log('inserted successfully!');
-			}
-		});
-	});
+// hàm xử lý khi server nhận tệp tin từ GPRS module
+tcpServer.on("connection", function(socket) {
+    console.log("tcpServer connected");
+    socket.on("data", function(msg) {
+
+        // xử lý để chuyển tin nhắn vừa nhận thành chuỗi JSON
+        var message = msg.toString();
+        var messageObj = JSON.parse(message);
+
+        // gửi Object vừa đóng gói lên trang liveUpdates
+        global.socketIO.emit('liveUpdates', messageObj);
+
+        // kết nối vào collection status và insert message từ website vào database
+        myDB.collection("status").insertOne(messageObj, function(err, res) {
+            if (!err) {
+                console.log('inserted successfully!');
+            }
+        });
+    });
 });
 
-tcpServer.listen(process.env.PORT || 1334, function(){
-	console.log("tcpServer listen on 1334 port");
+// tcp Server lắng nghe ở port 1334
+tcpServer.listen(process.env.PORT || 1334, function() {
+    console.log("tcpServer listen on 1334 port");
 });
 
+// khởi tạo kết nối đến database
 require('./connection.js');
 
 const app = express();
@@ -34,83 +44,132 @@ app.use(cors());
 
 app.use('/public', express.static('public'));
 
-app.get('/', function(req,res){
-  res.sendFile(__dirname + '/test.html');    // gửi file html lên máy
+// gửi file test.html khi có truy cập vào đường link ../
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/test.html');
 });
 
-app.get('/googlemap',(req,res)=>{
-	res.sendFile(__dirname+ '/googlemap.html');
+// gửi file googlemap.html khi có truy cập vào đường link ../googlemap
+app.get('/googlemap', (req, res) => {
+    res.sendFile(__dirname + '/googlemap.html');
 });
 
-app.get('/chart',(req,res)=>{
-	res.sendFile(__dirname+ '/ChartJsTest.html');
+// gửi file CharJsTest.html khi có truy cập vào đường link ../chart
+app.get('/chart', (req, res) => {
+    res.sendFile(__dirname + '/ChartJsTest.html');
 });
 
-app.get('/liveUpdates',(req,res)=>{
-	res.sendFile(__dirname+ '/liveUpdates.html');
-});	
+// gửi file liveUpdates.html khi có truy cập vào đường link ../liveUpdates
+app.get('/liveUpdates', (req, res) => {
+    res.sendFile(__dirname + '/liveUpdates.html');
+});
+
 // create a server for Socket.io
 const server = http.Server(app);
 
 // khởi tạo thư viện socket.io
 const io = SocketIO(server);
 
-io.on('connection',function(socket) {	// đoạn chương trình sẽ chạy khi có một kết nối đến server 
-	global.socketIO= socket;			
-	console.log('socket connection');			// in ra màn hình để debug
-	socket.on('customEvent',function(msg){		// nhận tập tin customEvent từ ESP8266
-		console.log('ok web');
-		var time = new Date().getTime();		// tạo thêm hàm thời gian để đưa lên server
-		msg.time = time;
-		
-		socket.broadcast.emit('liveUpdates',msg); // gửi tệp vừa đóng gói lên trang liveUpdates
-		// insert sample data to DB
-		myDB.collection("status").insertOne(msg, function(err, res) { // kết nối vào collection status và insert message từ website vào database
-			if (!err) {
-				console.log('inserted successfully!');
-			}
-		});
-	});
-	socket.on('chartemit',function(msg){		// xử lý khi nhận được tín hiệu chartemit 
-		var timeFrom = new Date(2018,3,msg.from[0]  ,msg.from[1],msg.from[2],0,0).getTime(); // xử lý ngày tháng nhận được ra định dạng thời gian chuẩn
-		var timeTo 	 = new Date(2018,3,msg.to[0],msg.to[1],msg.to[2],0,0).getTime();
-		
-		myDB.collection("status").find({ $and: [{time: {$gte : Number(timeFrom)}},{time: {$lte : Number(timeTo) } }]}).toArray(function(err, result){  //lấy tất cả dữ liệu nằm trong khoản thời gian cần tìm
-					console.log(result[msg.sensor]);
-					var dataEmit=[];
-					for (i=0; i < result.length ;i++){			// lọc riêng dữ liệu của sensor đã request 
-						dataEmit.push(result[i][msg.sensor]);
-					} 
-					socket.emit('updateChartJs',dataEmit); // emit gói updatechartJs với dữ liệu đã lọc
+// đoạn chương trình sẽ chạy khi có một kết nối đến server
+io.on('connection', function(socket) {
+    global.socketIO = socket;
+    console.log('socket connection');
 
-		});
-	});
+    // nhận tập tin customEvent từ ESP8266	
+    socket.on('customEvent', function(msg) {
+        console.log('ok web');
 
-	socket.on('UpdateGGmap',function(msg){  // xử lý khi nhận request từ trang googlemap
-		var timeFrom = new Date(2018,3,msg.from[0]  ,msg.from[1],msg.from[2],0,0).getTime();
-		var timeTo 	 = new Date(2018,3,msg.to[0] ,msg.to[1],msg.to[2],0,0).getTime();
-		console.log([timeFrom,timeTo]);
-		myDB.collection("status").find({ $and: [{time: {$gte : Number(timeFrom)}},{time: {$lte : Number(timeTo) } }]}).toArray(function(err, result){  //lấy tất cả file trong collection customers
-			if (!err) {
-				var kinh=[];
-				var vi=[];
-				for (i=0; i < result.length ;i++){
-					
-					kinh.push(result[i].GPS[0]);
-					vi.push(result[i].GPS[1]);
-				}
-				//console.log({"Kinhdo": kinh,"vido": vi});
-				socket.emit('updateGPS',{"kinhdo": kinh,"vido": vi})
-			}
-		});
-		
-	});
+        // tạo thêm hàm thời gian để đưa lên server
+        var time = new Date().getTime();
+        msg.time = time;
+
+        // gửi tệp vừa đóng gói lên trang liveUpdates
+        socket.broadcast.emit('liveUpdates', msg);
+
+        // sao chép vào database
+        myDB.collection("status").insertOne(msg, function(err, res) { // kết nối vào collection status và insert message từ website vào database
+            if (!err) {
+                console.log('inserted successfully!');
+            }
+        });
+    });
+
+    // xử lý khi nhận được tín hiệu chartemit 
+    socket.on('chartemit', function(msg) {
+
+        // xử lý ngày tháng nhận được ra định dạng thời gian chuẩn
+        var timeFrom = new Date(2018, 3, msg.from[0], msg.from[1], msg.from[2], 0, 0).getTime();
+        var timeTo = new Date(2018, 3, msg.to[0], msg.to[1], msg.to[2], 0, 0).getTime();
+
+        // tìm data theo thời gian vừa xử lý
+        myDB.collection("status").find({
+            $and: [{
+                time: {
+                    $gte: Number(timeFrom)
+                }
+            }, {
+                time: {
+                    $lte: Number(timeTo)
+                }
+            }]
+        }).toArray(function(err, result) { //lấy tất cả dữ liệu nằm trong khoản thời gian cần tìm
+            console.log(result[msg.sensor]);
+            var dataEmit = [];
+
+            // lọc riêng dữ liệu của sensor đã request 
+            for (i = 0; i < result.length; i++) {
+                dataEmit.push(result[i][msg.sensor]);
+            }
+
+            // emit gói updatechartJs với dữ liệu đã lọc
+            socket.emit('updateChartJs', dataEmit);
+
+        });
+    });
+
+    // xử lý khi nhận request từ trang googlemap
+    socket.on('UpdateGGmap', function(msg) {
+
+        // xử lý ngày tháng nhận được ra định dạng thời gian chuẩn
+        var timeFrom = new Date(2018, 3, msg.from[0], msg.from[1], msg.from[2], 0, 0).getTime();
+        var timeTo = new Date(2018, 3, msg.to[0], msg.to[1], msg.to[2], 0, 0).getTime();
+        console.log([timeFrom, timeTo]);
+
+        // tìm data theo thời gian vừa xử lý
+        myDB.collection("status").find({
+            $and: [{
+                time: {
+                    $gte: Number(timeFrom)
+                }
+            }, {
+                time: {
+                    $lte: Number(timeTo)
+                }
+            }]
+        }).toArray(function(err, result) { //lấy tất cả file trong collection customers
+            if (!err) {
+                var kinh = [];
+                var vi = [];
+                for (i = 0; i < result.length; i++) {
+
+                    kinh.push(result[i].GPS[0]);
+                    vi.push(result[i].GPS[1]);
+                }
+
+                socket.emit('updateGPS', {
+                    "kinhdo": kinh,
+                    "vido": vi
+                })
+            }
+        });
+
+    });
 });
 
-
-process.once('dbReady', () => {   // một khi đã kết nói với database mới khởi động server
-	myDB = global.connection.db('prodata');
-	server.listen(process.env.PORT || 1333, () => {
-		console.log('Server started on port 3000...');
-	});
+// một khi đã kết nói với database mới khởi động server
+process.once('dbReady', () => {
+    myDB = global.connection.db('prodata');
+    server.listen(process.env.PORT || 1333, () => {
+        console.log('Server started on port 1333...');
+    });
 });
